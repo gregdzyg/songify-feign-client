a73610ff065cc935a50f01aa3a663772b9aa4e5e
Grzegorz Dżyg <grzegorzdzyg@gregMc.local>
2025-10-16 12:02:56 +0200

    Initial commit

---
+++ src/main/java/com/songifyfeignclient/SongifyFeignClientApplication.java
@@ -0,0 +1,13 @@
+package com.songifyfeignclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongifyFeignClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SongifyFeignClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/songifyfeignclient/SongifyFeignClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.songifyfeignclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongifyFeignClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
