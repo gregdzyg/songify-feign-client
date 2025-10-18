32b90ff9cfa549402fb9f02dc7baebf37e42b167
Grzegorz Dżyg <grzegorzdzyg@gregMc.local>
2025-10-18 13:10:23 +0200

    add exception handling

--- src/main/java/com/songifyfeignclient/SongifyFeignClientApplication.java
+++ src/main/java/com/songifyfeignclient/SongifyFeignClientApplication.java
@@ -2,6 +2,9 @@ package com.songifyfeignclient;
 
 import com.songifyfeignclient.service.SongifyClientService;
 import com.songifyfeignclient.domain.Song;
+import feign.FeignException;
+import feign.RetryableException;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
@@ -9,6 +12,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.event.EventListener;
 @EnableFeignClients
 @SpringBootApplication
+@Log4j2
 public class SongifyFeignClientApplication {
 
     SongifyClientService songifyClientService;
@@ -25,14 +29,26 @@ public class SongifyFeignClientApplication {
     @EventListener(ApplicationStartedEvent.class)
     public void run() {
 
-        songifyClientService.makeGetAllSongsRequest();
-        songifyClientService.makeGetSongByIdRequest();
-        songifyClientService.makeCreateSongRequest(new Song("Classic song", "Chopin"));
-        songifyClientService.makeGetAllSongsRequest();
-        songifyClientService.makeDeleteSongByIdRequest(1);
-        songifyClientService.makeGetAllSongsRequest();
-        songifyClientService.makeUpdateSongRequest(5, new Song("Random song", "Random artist"));
-        songifyClientService.makeGetAllSongsRequest();
-       // songifyClientService.makePartiallyUpdateSongRequest(6, new Song(null, "Fryderyk"));
+        try {
+            songifyClientService.makeGetAllSongsRequest();
+            songifyClientService.makeGetSongByIdRequest();
+            songifyClientService.makeCreateSongRequest(new Song("Classic song", "Chopin"));
+            songifyClientService.makeGetAllSongsRequest();
+            songifyClientService.makeDeleteSongByIdRequest(1);
+            songifyClientService.makeGetAllSongsRequest();
+            songifyClientService.makeUpdateSongRequest(5, new Song("Random song", "Random artist"));
+            songifyClientService.makeGetAllSongsRequest();
+            // songifyClientService.makePartiallyUpdateSongRequest(6, new Song(null, "Fryderyk"));
+        } catch (FeignException.FeignClientException feignClientException) {
+            log.error("Client error: {}",feignClientException.getMessage());
+        } catch (FeignException.FeignServerException feignServerException) {
+            log.error("Server error: {}", feignServerException.getMessage());
+        } catch (RetryableException retryableException) {
+            log.error("Connection error: {} {}", retryableException.status(),
+                    retryableException.getMessage());
+        } catch (FeignException feignException) {
+            log.error("Something went wrong: {} {}", feignException.status(),
+                    feignException.getMessage());
+        }
     }
 }
